17a75b1656cb55fa9c30b7ba7b65865eaeb3c85e
Tigran Tovmasyan <tigrantovmasyan@live.com>
2026-01-29 18:30:39 +0400

    feat: connect stars with lines to complete Orion constellation

--- orion.py
+++ orion.py
@@ -67,7 +67,41 @@ turtle.write("Saiph")
 turtle.goto(RIGHT_KNEE_X, RIGHT_KNEE_Y)             # Right knee
 turtle.write("Rigel")
 
+# Draw a line from the left shoulder to the left star of the belt
+turtle.goto(LEFT_SHOULDER_X, LEFT_SHOULDER_Y)
+turtle.pendown()
+turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+turtle.penup()
 
+# Draw a line from the right shoulder to the right star of the belt
+turtle.goto(RIGHT_SHOULDER_X, RIGHT_SHOULDER_Y)
+turtle.pendown()
+turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+turtle.penup()
+
+# Draw a line from the left star of the belt to the middle star of the belt
+turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+turtle.pendown()
+turtle.goto(MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
+turtle.penup()
+
+# Draw a line from middle star of the belt to the right star of the belt
+turtle.goto(MIDDLE_BELTSTAR_X, MIDDLE_BELTSTAR_Y)
+turtle.pendown()
+turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+turtle.penup()
+
+# Draw a line from the left star of the belt to the left knee
+turtle.goto(LEFT_BELTSTAR_X, LEFT_BELTSTAR_Y)
+turtle.pendown()
+turtle.goto(LEFT_KNEE_X, LEFT_KNEE_Y)
+turtle.penup()
+
+# Draw a line from the right star of the belt to the right knee
+turtle.goto(RIGHT_BELTSTAR_X, RIGHT_BELTSTAR_Y)
+turtle.pendown()
+turtle.goto(RIGHT_KNEE_X, RIGHT_KNEE_Y)
+turtle.penup()
 
 
 # Leave the window open
