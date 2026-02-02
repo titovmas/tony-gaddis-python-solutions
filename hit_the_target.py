53d4e394f5514b007219421bee99f6e2cb5b04e9
Tigran Tovmasyan <tigrantovmasyan@live.com>
2026-02-02 20:05:56 +0400

    initial commit: add named constants for screen and target

---
+++ hit_the_target.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import turtle
+
+# Named Constants
+
+# Screen parameters
+SCREEN_WIDTH = 600      # Screen width
+SCREEN_HEIGHT = 600     # Screen height
+
+# Target parameters
+TARGET_LLEFT_X = 100    # Target bottom_left X coordinate
+TARGET_LLEFT_Y = 250    # Target bottom_left Y coordinate
+TARGET_WIDTH = 25       # Target width
+
+# Physics and animation
+FORCE_FACTOR = 30       # Arbitary force factor
+PROJECTILE_SPEED = 1    # Projectile animation speed
+
+#Directions (in degrees)
+NORTH = 90              # North direction angle
+SOUTH = 270             # South direction angle
+EAST = 0                # East direction angle
+WEST = 180              # West direction angle
